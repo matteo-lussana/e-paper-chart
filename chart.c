d8f12b1a5e15b2d8d82c45ddeb31c0840d97a911
Matteo Lussana <72503717+matteo-lussana@users.noreply.github.com>
2026-06-18 22:04:39 +0200

    feat: implementation of draw_hline, draw_vline

--- chart.c
+++ chart.c
@@ -32,5 +32,38 @@ void set_pixel(uint8_t *buf, int x, int y, int color)
     buf[byte_index] |= (1 << bit_index);
   else
     buf[byte_index] &= ~(1 << bit_index);
-
 }
+
+void draw_hline(uint8_t *buf, int x0, int x1, int y, int thickness, int color)
+{
+  int up, down;
+  if(thickness % 2 == 1){
+    up = down = (thickness - 1) / 2;
+  } else{
+    up = down = thickness / 2;
+    down--;
+  }
+
+  for(int c = x0; c <= x1; c++){
+    for(int r = y - up; r <= y + down; r++){
+      set_pixel(buf, c, r, color);
+    }
+  }
+}
+void draw_vline(uint8_t *buf, int x,  int y0, int y1, int thickness, int color)
+{
+  int left, right;
+  if(thickness % 2 == 1){
+    left = right = (thickness - 1) / 2;
+  } else{
+    left = right = thickness / 2;
+    right--;
+  }
+
+  for(int r = y0; r <= y1; r++){
+    for(int c = x - left; c <= x + right; c++){
+      set_pixel(buf, c, r, color);
+    }
+  }
+}
+
--- chart.h
+++ chart.h
@@ -8,3 +8,5 @@
 
 void save_pbm(const char *filename, uint8_t *buf);
 void set_pixel(uint8_t *buf, int x, int y, int color);
+void draw_hline(uint8_t *buf, int x0, int x1, int y, int thickness, int color);
+void draw_vline(uint8_t *buf, int x,  int y0, int y1, int thickness, int color);
