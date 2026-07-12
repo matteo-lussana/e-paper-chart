63f88647098de7f4fb5e981d442aafdc3a2cf4d0
Matteo Lussana <72503717+matteo-lussana@users.noreply.github.com>
2026-07-12 10:21:47 +0200

    docs: add comment to every function in chart.h

--- chart.h
+++ chart.h
@@ -89,7 +89,13 @@ typedef struct{
 #ifdef __cplusplus
 extern "C" {
 #endif
-
+/********************** UTILITY ***************************/
+/**
+ * Save the framebuffer in a Portable BitMap file (.bpm). Used for test purpose.
+ *
+ * @param filename      name of the file
+ * @param buf           framebuffer
+ * /
 void save_pbm(const char *filename, uint8_t *buf);
 /**
  * It set the color of a single pixel in the framebuffer
@@ -131,23 +137,145 @@ void draw_hline(uint8_t *buf, int x0, int x1, int y, int thickness, Color color,
  * @param color         line color (COLOR_WHITE = 0, COLOR_BLACK = 1, COLOR_GRAY = 2)
  * @param type          line type (LINE_SOLID  = 0, LINE_DASHED = 1, LINE_DOTTED = 2)
  */
-
 void draw_vline(uint8_t *buf, int x, int y0, int y1, int thickness, Color color, LineType type);
+
+/**
+ * Draw line in every Direction
+ *
+ * @param buf           framebuffer
+ * @param x0, y0        starting point
+ * @param x1, y1        final point
+ * @param thickness     thickness of the line
+ * @param color         line color (COLOR_WHITE = 0, COLOR_BLACK = 1, COLOR_GRAY = 2)
+ * @param type          line type (LINE_SOLID  = 0, LINE_DASHED = 1, LINE_DOTTED = 2)
+ */
 void draw_line(uint8_t *buf, int x0, int y0, int x1, int y1, int thickness, Color color, LineType type);
+
+/**
+ * Draw a rectangle
+ *
+ * @param buf           framebuffer
+ * @param x0, y0        starting point
+ * @param x1, y1        final point
+ * @param thickness     thickness of the line
+ * @param color         line color (COLOR_WHITE = 0, COLOR_BLACK = 1, COLOR_GRAY = 2)
+ * @param type          line type (LINE_SOLID  = 0, LINE_DASHED = 1, LINE_DOTTED = 2)
+ */
 void draw_rect(uint8_t *buf, int x0, int x1, int y0, int y1, int thickness, Color color, LineType line_type);
+
+/**
+ * Fill a rectangle
+ *
+ * @param buf           framebuffer
+ * @param x0, y0        starting point
+ * @param x1, y1        final point
+ * @param color         line color (COLOR_WHITE = 0, COLOR_BLACK = 1, COLOR_GRAY = 2)
+ */
 void fill_rect(uint8_t *buf, int x0, int x1, int y0, int y1, Color color);
+
+/**
+ * Draw a circle
+ *
+ * @param buf           framebuffer
+ * @param cx, cy        center coordinates
+ * @param radius        circle radius
+ * @param thickness     thickness of the line
+ * @param color         line color (COLOR_WHITE = 0, COLOR_BLACK = 1, COLOR_GRAY = 2)
+ */
 void draw_circle(uint8_t *buf, int cx, int cy, int radius, int thickness, Color color);
 
-//text
+
+/********************** TEXT ***************************/
+/**
+ * Write single character
+ *
+ * @param buf           framebuffer
+ * @param x, y          starting coordinates
+ * @param c             character to write
+ * @param color         line color (COLOR_WHITE = 0, COLOR_BLACK = 1, COLOR_GRAY = 2)
+ * @param scale         size of the character (> 0)
+ * @param rotation      rotation of the char clockwise (0, 90, 180, 270)
+ */
 void draw_char(uint8_t *buf, int x, int y, char c, Color color, int scale, int rotation);
+
+/**
+ * Write entire string
+ *
+ * @param buf           framebuffer
+ * @param x, y          starting coordinates
+ * @param str           string to write
+ * @param color         line color (COLOR_WHITE = 0, COLOR_BLACK = 1, COLOR_GRAY = 2)
+ * @param scale         size of the character (> 0)
+ * @param rotation      rotation of the char clockwise (0, 90, 180, 270)
+ */
 void draw_text(uint8_t *buf, int x, int y, const char *str, Color color, int scale, int rotation);
 
-//charts
+/********************** CHARTS ***************************/
+/**
+ * Draw bar chart
+ *
+ * @param buf           framebuffer
+ * @param cfg           config for the bar char (see BarChartConfig)
+ * @param x_data        labels for the x-axis
+ * @param y_data        actual data of the chart
+ * @param n             lenght of the x_data and y_data
+ */
 void draw_bar_chart(uint8_t *buf, const BarChartConfig *cfg, char **x_data, float *y_data, int n);
+
+/**
+ * Draw bar chart with multiple bars
+ *
+ * @param buf           framebuffer
+ * @param cfg           config for the bar char (see BarChartConfig)
+ * @param x_data        labels for the x-axis
+ * @param y_data        actual data of the chart
+ * @param data_length   lenght of the x_data and y_data
+ * @param data_sets     number of the datasets. number of column per label
+ */
 void draw_multi_bar_chart(uint8_t *buf, const BarChartConfig *cfg, char **x_data, float **y_data, int data_length, int data_sets);
+
+/**
+ * Draw bar chart with two y-axis and two bars
+ *
+ * @param buf           framebuffer
+ * @param cfg           config for the bar char (see DoubleAxisBarChartConfig)
+ * @param x_data        labels for the x-axis
+ * @param y_data_left   actual data of the chart for the left y-axis
+ * @param y_data_right  actual data of the chart for the left x-axis
+ * @param data_length   lenght of the x_data, y_data_left, and y_data_right
+ */
 void draw_double_axis_bar_chart(uint8_t *buf, const DoubleAxisBarChartConfig *cfg, char **x_data, float *y_data_left, float *ydata_right, int data_length);
+
+/**
+ * Draw line chart
+ *
+ * @param buf           framebuffer
+ * @param cfg           config for the bar char (see LineChartConfig)
+ * @param x_data        labels for the x-axis
+ * @param y_data        actual data of the chart
+ * @param n             lenght of the x_data and y_data
+ */
 void draw_line_chart(uint8_t *buf, const LineChartConfig *cfg, char **x_data, float *y_data, int n);
+
+/**
+ * Draw pie chart
+ *
+ * @param buf           framebuffer
+ * @param cfg           config for the bar char (see PieChartConfig)
+ * @param x_data        labels for the slice
+ * @param y_data        actual data of the chart
+ * @param n             lenght of the x_data and y_data
+ */
 void draw_pie_chart(uint8_t *buf, const PieChartConfig *cfg, char **x_data, float *y_data, int n);
+
+/**
+ * Draw heat-map/frequency chart
+ *
+ * @param buf           framebuffer
+ * @param cfg           config for the bar char (see PieChartConfig)
+ * @param data          actual data of the chart. array with 0, 1, 2 for colorign white, black, gray every square
+ * @param n             lenght of the data. number of squares
+ */
 void draw_freq_chart(uint8_t *buf, const FreqChartConfig *cfg, int *data, int n);
 
 #ifdef __cplusplus
